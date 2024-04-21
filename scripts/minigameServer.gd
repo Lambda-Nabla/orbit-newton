7cb13468a7f1b0bca82de9681b2ce687326b5740
Eva <evapowlison@protonmail.com>
2024-04-20 21:33:00 -0400

    add minigameServer

---
+++ scripts/minigameServer.gd
@@ -0,0 +1,17 @@
+extends Node
+
+var minigames = []
+var tree: SceneTree
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	minigames.append("res://scenes/wires_minigame.tscn")
+	minigames.append("res://scenes/terminal minigame.tscn")
+	minigames.append("res://scenes/pressureminigame.tscn")
+
+func serve_minigame():
+	var minigame = minigames.pick_random()
+	tree.change_scene_to_file(minigame)
+
+func reload_main_scene():
+	tree.change_scene_to_file("res://scenes/main.tscn")
