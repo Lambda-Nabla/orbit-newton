06f606220e26511e2acac569a5e0c436123fe86f
WaddlesPF <113570007+WaddlesPF@users.noreply.github.com>
2024-04-21 11:20:42 -0400

    hud fix

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -4,6 +4,7 @@ var lHealth
 var lVelocity
 var lGravity
 var lNearestBody
+var lScore
 
 var player
 
@@ -12,16 +13,20 @@ var velocity
 var gravity
 var nearest
 
+var minigameServer
+
 var orangeSpeed
 var redSpeed
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	minigameServer = get_node("/root/MinigameServer")
 	lHealth = get_node("Grid/Health")
 	lVelocity = get_node("Grid/Velocity")
 	lGravity = get_node("Grid/Gravity")
 	lNearestBody = get_node("Grid/DistNearestBody")
+	lScore = get_node("You win!/score")
 	
 	player = get_node("/root/Node2D/Player")
 
@@ -33,7 +38,7 @@ func _process(delta):
 	lVelocity.text = str("Velocity: ", velocity)
 	lGravity.text = str("Gravity: ", gravity)
 	lNearestBody.text = str("Distance to nearest body: ", nearest)
-	
+	lScore.text = str("Score: ", minigameServer.score)
 	if abs(velocity.x) > redSpeed or abs(velocity.y) > redSpeed:
 		lVelocity.set("theme_override_colors/font_color", Color(1,0,0))
 	elif abs(velocity.x) > orangeSpeed or abs(velocity.y) > orangeSpeed:
@@ -41,7 +46,7 @@ func _process(delta):
 	else:
 		lVelocity.set("theme_override_colors/font_color", Color(0,1,0))
 		
-	if get_node("/root/MinigameServer").score == 3:
+	if minigameServer.score >= 3:
 		$"You win!".show()
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		
@@ -57,3 +62,7 @@ func _getPlayerVars():
 		velocity = player.velocity
 		gravity = player.playerGravity
 		nearest = player.get_nearest_body()
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	minigameServer.score = 0
