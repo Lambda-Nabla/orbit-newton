043f381831cac7e1c8a6a5f17085f930145c6929
WaddlesPF <113570007+WaddlesPF@users.noreply.github.com>
2024-04-21 11:46:51 -0400

    fixed hud issue

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -38,7 +38,7 @@ func _process(delta):
 	lVelocity.text = str("Velocity: ", velocity)
 	lGravity.text = str("Gravity: ", gravity)
 	lNearestBody.text = str("Distance to nearest body: ", nearest)
-	lScore.text = str("Score: ", 3 - minigameServer.playerHealth)
+	lScore.text = str("Score (health you ended with): ", minigameServer.playerHealth)
 	if abs(velocity.x) > redSpeed or abs(velocity.y) > redSpeed:
 		lVelocity.set("theme_override_colors/font_color", Color(1,0,0))
 	elif abs(velocity.x) > orangeSpeed or abs(velocity.y) > orangeSpeed:
